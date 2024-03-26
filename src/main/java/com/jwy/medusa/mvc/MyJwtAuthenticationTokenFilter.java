724289be15f3ae54499ebd363644c9b3a5c87c21
jiangwanyu <jiangwanyu@yanxiu.com>
2024-03-26 17:04:39 +0800

    jwt

--- src/main/java/com/jwy/medusa/mvc/MyJwtAuthenticationTokenFilter.java
+++ src/main/java/com/jwy/medusa/mvc/MyJwtAuthenticationTokenFilter.java
@@ -12,9 +12,12 @@
 package com.jwy.medusa.mvc;
 
 import com.jwy.medusa.common.exception.UnauthorizedException;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.util.PathMatcher;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -22,6 +25,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 /**
  * <p>
@@ -36,10 +40,9 @@ import java.io.IOException;
  * @version 1.0
  * @date 2024/2/26
  */
+@Slf4j
 @Component
 public class MyJwtAuthenticationTokenFilter extends OncePerRequestFilter {
-
-    private final String AUTH_HEADER = "authorization";
 
     @Value("${my.jwt.token:123123}")
     private String jwtToken;
@@ -47,8 +50,12 @@ public class MyJwtAuthenticationTokenFilter extends OncePerRequestFilter {
     @Value("${my.authorization.jwt.enabled:true}")
     private boolean enabled;
 
-    @Value("${my.authorization.path.ignores:}")
-    private CharSequence[] ignoresArr;
+    @Value("#{'${my.authorization.path.ignores:}'.split(',')}")
+    private List<String> ignoreUrlList;
+
+    private final String AUTH_HEADER = "authorization";
+
+    private static PathMatcher matcher = new AntPathMatcher();
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
@@ -63,13 +70,23 @@ public class MyJwtAuthenticationTokenFilter extends OncePerRequestFilter {
         filterChain.doFilter(request, response);
     }
 
-
     @Override
     protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
 
         if(!enabled) return true;
 
-        String url = request.getRequestURI();
-        return StringUtils.startsWithAny(url, "/actuator", "/swagger", "/v3/api-docs") || StringUtils.startsWithAny(url, ignoresArr);
+        final String _url = request.getRequestURI();
+
+        boolean res = matcher.match("/feign/**", _url) ||
+                matcher.match("/actuator/**", _url) ||
+                matcher.match("/swagger*/**", _url) ||
+                matcher.match("/v3/api-docs/**", _url);
+
+        for (String ignoreUrl : ignoreUrlList) {
+            if(StringUtils.isEmpty(ignoreUrl)) continue;
+            if(res || matcher.match(ignoreUrl, _url)) return true;
+        }
+
+        return res;
     }
 }
